07f374f79b0823da3ebeb0a5bc8687a334d9e106
Isabel Villa Ruiz <ivilla@delta10.aulas.gsyc.urjc.es>
2019-02-04 12:53:10 +0100

    pruebitas

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -1,11 +1,24 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
+var fs = require('fs');
+var url = require('url');
+//var dt = require('./myfirstmodule');
 
 console.log("Arrancando servidor...")
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime() + "\n\n");
-  res.end('Hello Kaamoo!');
-  console.log("Peticion atendida")
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  console.log("filename" + filename);
+    fs.readFile(filename, function(err,data){
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      //res.write("The date and time are currently: " + dt.myDateTime() + "\n\n");
+      //res.end('Hello Kaamoo!');
+      res.write(data);
+      return res.end();
+      console.log("Peticion atendida")
+    });
 }).listen(8080);
