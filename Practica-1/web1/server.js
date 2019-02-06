7d2ecc917e04bb06537a64b5039e1bebac955227
isabelvillaruiz <iissaavilla@gmail.com>
2019-02-06 12:30:17 +0100

    "actualizando"

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var n = 1;
 //var dt = require('./myfirstmodule');
 
 console.log("Arrancando servidor...")
@@ -8,9 +9,29 @@ console.log("Arrancando servidor...")
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
-  console.log("filename" + filename);
+  var index = "./";
+  console.log("filename: " + filename);
+
+  if ( filename == index) {
+    console.log("HE ENTRADO");
+    fs.readFile("data1.html", function(err,data){
+      // if (err) {
+      //   console.log( "err" + err);
+      //   res.writeHead(404, {'Content-Type': 'text/html'});
+      //   return res.end("404 Not Found");
+      // }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      //res.write("The date and time are currently: " + dt.myDateTime() + "\n\n");
+      //res.end('Hello Kaamoo!');
+      res.write(data);
+      return res.end();
+      console.log("Peticion atendida")
+    });
+  }else{
     fs.readFile(filename, function(err,data){
+      console.log("data" + data);
       if (err) {
+        console.log( "err" + err);
         res.writeHead(404, {'Content-Type': 'text/html'});
         return res.end("404 Not Found");
       }
@@ -21,4 +42,5 @@ http.createServer(function (req, res) {
       return res.end();
       console.log("Peticion atendida")
     });
+  }
 }).listen(8080);
