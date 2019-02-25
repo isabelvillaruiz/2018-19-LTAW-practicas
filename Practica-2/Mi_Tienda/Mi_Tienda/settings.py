899fc6906a20cb402db8428c53b63177575f3622
isabelvillaruiz <iissaavilla@gmail.com>
2019-02-25 12:48:09 +0100

    Probando en windows

--- Practica-2/Mi_Tienda/Mi_Tienda/settings.py
+++ Practica-2/Mi_Tienda/Mi_Tienda/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'Mi_Tienda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/alumnos/ivilla/github/2018-19-LTAW-practicas/Practica-2/Mi_Tienda/Mi_Tienda/TEMPLATES'],
+        'DIRS': ['C:\Users\Isabel\Documents\ISAM\CUARTO\\2CUATRIMESTRE\LTAW\\2018-19-LTAW-practicas\Practica-2\Mi_Tienda\Mi_Tienda\TEMPLATES',
+                '/home/alumnos/ivilla/github/2018-19-LTAW-practicas/Practica-2/Mi_Tienda/Mi_Tienda/TEMPLATES'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
