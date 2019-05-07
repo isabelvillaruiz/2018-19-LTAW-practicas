8c3c938a0b4a2b1492abefb796c554b40af1ec29
isabelvillaruiz <iissaavilla@gmail.com>
2019-05-07 20:37:55 +0200

    Working on forms and views.

--- Practica-2/mi_proyectoweb_tiendatombow/mi_tienda/models.py
+++ Practica-2/mi_proyectoweb_tiendatombow/mi_tienda/models.py
@@ -7,3 +7,8 @@ class Product (models.Model):
     name = models.CharField(max_length=200)
     stock = models.IntegerField()
     price = models.FloatField()
+
+class Order (models.Model):
+    Name = models.CharField(max_length=20)
+    Address = models.CharField(max_length=100)
+    Wishes = models.CharField(max_length=400)
--- Practica-2/mi_proyectoweb_tiendatombow/mi_tienda/views.py
+++ Practica-2/mi_proyectoweb_tiendatombow/mi_tienda/views.py
@@ -6,6 +6,21 @@ from django.http import HttpResponse
 
 from mi_tienda.models import Product
 
+
+PLANTILLA_LIST = """
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<link rel="stylesheet" href="{% static 'styles.css'%}">
+  <head>
+    <meta charset="utf-8">
+    <title>Listado</title>
+  </head>
+  <body>
+    <p>Bienvenido a mi tienda, {{user}}</p>
+  </body>
+</html>
+"""
+
 # Create your views here.
 def home_view (request):
     return render(request, "index.html", {})
@@ -20,9 +35,17 @@ def order_view (request):
     return render(request, "order.html", {})
 
 def list(request):
+
+
+    products_to_show = []
     objects = Product.objects.all()
-    html = "<p>Listado de articulos</p>"
+
     for elt in objects:
-        print(elt.name)
-        html += '<p>'+ elt.name + ' ' + str(elt.price) + '<p>'
+        products_to_show.append(elt)
+
+    return render(request,'list.html',{'prod_list':products_to_show})
+
+
+    # -- Obtener la pagina html final
+    html = t.render(c)
     return HttpResponse(html)
