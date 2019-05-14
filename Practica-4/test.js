61ca9ce71c357bd90419d8197433e85e57448633
isabelvillaruiz <iissaavilla@gmail.com>
2019-05-14 21:40:22 +0200

    P4 finished :D

--- Practica-4/test.js
+++ Practica-4/test.js
@@ -2,6 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+const express = require('express')
+
+var users = 0;
+
+//app.use(express.static('public'));
+
+
 //--Servir la pagina principal
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,6 +21,7 @@ app.get('/chat-client.js', function(req, res){
   console.log("Fichero js solicituado")
 });
 
+
 //-- Lanzar el servidor
 http.listen(3000, function(){
   console.log('listening on *:3000');
@@ -24,20 +32,63 @@ http.listen(3000, function(){
 //-- Un nuevo cliente se ha conectado!
 io.on('connection', function(socket){
   console.log('--> Usuario conectado!');
+  answer = '<br> NEW USER CONCCECTED! (ง^ᗜ^)ง" </br>'
+  io.emit('new_message',answer);
+
+  answer = '<br>  Welcome to the chat!! ٩( ᐛ )و " </br>'
+  socket.emit('new_message',answer);
+
+  users = users + 1;
 
   //-- Detectar si el usuario se ha desconectado
   socket.on('disconnect', function(){
     console.log('--> Usuario Desconectado');
+    users = users -1;
   });
 
   //-- Detectar si se ha recibido un mensaje del cliente
   socket.on('new_message', msg => {
 
-    //-- Notificarlo en la consola del servidor
-    console.log("Mensaje recibido: " + msg)
+    console.log("msg", msg)
+    if (msg == '/help') {
 
-    //-- Emitir un mensaje a todos los clientes conectados
-    io.emit('new_message', msg);
+      console.log("Holi")
+
+      answer = '<br> Esta es la lista de comandos que puedes ejcutar: </br>' +
+        '<ul>'+
+        '<li>/help</li>'+
+        '<li>/list</li>'+
+        '<li>/hello</li>'+
+        '<li>/date</li>'+
+        '</ul>'
+
+      socket.emit('new_message', answer)
+
+    }else if ( msg == '/list') {
+
+      answer = '<br> El numero de usuarios es  (~‾▿‾)~ ' +   users + ': </br>'
+
+      socket.emit('new_message', answer)
+
+    }else if ( msg == '/hello' ){
+
+      answer = '<br> Hi! Im the server (☞ﾟヮﾟ)☞  </br>'
+
+      socket.emit('new_message', answer)
+
+    }else if ( msg == '/date') {
+
+      var date = new Date();
+      answer = '<br> (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧ ' +  date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() +  '</br>'
+      socket.emit('new_message', answer)
+
+    }else{
+      //-- Notificarlo en la consola del servidor
+      console.log("Mensaje recibido: " + msg)
+
+      //-- Emitir un mensaje a todos los clientes conectados
+      io.emit('new_message', msg);
+    }
   })
 
 });
